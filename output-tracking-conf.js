e160780c7d695241d14ba7d8f8df54681d16d798
Boris SCHAPIRA <borisschapira@gmail.com>
2018-06-19 17:47:00 +0200

    Better output

--- output-tracking-conf.js
+++ output-tracking-conf.js
@@ -37,14 +37,17 @@ function flattenData(trackingData) {
 			trackingData.screenWidth = trackingData.screen.width;
 		}
 	}
+	if (trackingData.lastExecution) {
+		trackingData.lastExecution = new Date(trackingData.lastExecution).toISOString();
+	}
 	if (trackingData.header && trackingData.header.length > 0) {
 		const headerLength = trackingData.header.length;
 		for (let i = 0; i < headerLength; i++) {
 			trackingData['header_' + trackingData.header[i].key] = trackingData.header[i].value;
 		}
 	}
-	if (trackingData.id && trackingData.lang) {
-		trackingData.trackingUrl = 'https://www.dareboost.com/' + trackingData.lang + '/tracking/view/' + trackingData.id;
+	if (trackingData.id) {
+		trackingData.trackingUrl = 'https://www.dareboost.com/' + (trackingData.lang ? trackingData.lang : 'en') + '/tracking/edit/' + trackingData.id;
 	}
 
 	const pickedPropItem = _.extend({}, _.pick(trackingData, 'id', 'trackingUrl', 'url', 'name', 'state', 'lastExecution', 'lastExecutionReportUrl', 'lang', 'enabled', 'location', 'browserName', 'isMobile', 'bandwidthUpstream', 'bandwidthDownstream', 'latency', 'isPrivate', 'adblock', 'screenHeight', 'screenWidth'), _.pick(trackingData, (value, key) => {
