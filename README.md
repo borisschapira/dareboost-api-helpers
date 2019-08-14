775ccccff251c6f20dcf29153da381188a84ef10
Boris SCHAPIRA <borisschapira@gmail.com>
2019-08-14 10:46:35 +0200

    No yarn + add tests

--- README.md
+++ README.md
@@ -10,7 +10,6 @@ These instructions will get you a copy of the project up and running on your loc
 
 * [git](https://git-scm.com/)
 * node 8.11.3+ (may work on minor version but I've not tested)
-* [yarn package manager](https://yarnpkg.com/)
 
 For node, I recommend using [nvm](https://github.com/creationix/nvm) to switch from one node version to another.
 
@@ -25,7 +24,7 @@ git clone https://github.com/borisschapira/dareboost-api-helpers.git
 Then enter the folder and install the project using yarn:
 
 ```
-yarn install
+npm install
 ```
 
 ### Using the helpers
@@ -33,7 +32,7 @@ yarn install
 From now on, there is only one helper that exports a user's active tracking configuration to JSON and CSV. Don't forget to set [your Dareboost API token](https://www.dareboost.com/en/profile/api) (you need to be a customer, see [Dareboost's offers](https://www.dareboost.com/en/offers#gppufs)) right before calling the helper.
 
 ```
-DB_API_TOKEN='your_api_token' yarn run page:monitor
+DB_API_TOKEN='your_api_token' npm run page:monitor
 ```
 
 
@@ -44,12 +43,6 @@ Hey! Hum… I'm gonna write some tests. Some day. In the near future.
 ## Contributing
 
 Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
-
-Please ensure that your code does not triggered any linting alert:
-
-```
-yarn lint
-```
 
 ## Authors
 
---
+++ tests/transformations/flattenObjects.js
@@ -0,0 +1,106 @@
+const { expect } = require("chai");
+const { flattenObject, flattenObjects } = require("../../lib/transformations");
+
+describe("flattenObject", async () => {
+	it("flattens a standard object", async () => {
+		const source = {
+			key_one: "hello",
+			key_two: { subkey_one: "world", subkey_two: { x: 4 } },
+			key_three: [{ key: "the_key", value: "the_value" }]
+		};
+		const target = {
+			keyOne: "hello",
+			keyThree0Key: "the_key",
+			keyThree0Value: "the_value",
+			keyTwoSubkeyOne: "world",
+			keyTwoSubkeyTwoX: 4
+		};
+		expect(target).to.deep.equal(flattenObject(source));
+	});
+
+	it("flattens a monitor config", async () => {
+		const source = {
+			id: 7134,
+			url: "https://boris.schapira.dev/",
+			name: "boris.schapira.dev",
+			state: "OK",
+			lastExecution: 1565686301911,
+			enabled: true,
+			frequency: 1440,
+			lang: "fr",
+			config: {
+				location: "Paris",
+				browser: { name: "Chrome" },
+				isMobile: false,
+				bandwidth: { upstream: 1500, downstream: 8000 },
+				latency: 50,
+				isPrivate: false,
+				adblock: false,
+				screen: { height: 768, width: 1366 },
+				header: [
+					{
+						value:
+							"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36 DareBoost",
+						key: "User-Agent"
+					},
+					{ value: "cache", key: "pragma" }
+				],
+				cookies: [
+					{
+						name: "cname",
+						value: "cvalue",
+						domain: "boris.schapira.dev",
+						path: "/"
+					}
+				],
+				repeatedView: false,
+				http2Disabled: false
+			}
+		};
+		const target = {
+			id: 7134,
+			url: "https://boris.schapira.dev/",
+			name: "boris.schapira.dev",
+			state: "OK",
+			lastExecution: "2019-08-13T08:51:41.911Z",
+			enabled: true,
+			frequency: 1440,
+			lang: "fr",
+			configLocation: "Paris",
+			configBrowserName: "Chrome",
+			configIsMobile: false,
+			configBandwidthUpstream: 1500,
+			configBandwidthDownstream: 8000,
+			configLatency: 50,
+			configIsPrivate: false,
+			configAdblock: false,
+			configScreenHeight: 768,
+			configScreenWidth: 1366,
+			configHeaderUserAgent:
+				"User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36 DareBoost",
+			configHeaderPragma: "pragma: cache",
+			configCookieCname: "cname=cvalue; Path=/; Domain=boris.schapira.dev",
+			configRepeatedView: false,
+			configHttp2Disabled: false
+		};
+		expect(target).to.deep.equal(flattenObject(source));
+	});
+});
+
+describe("flattenObjects", async () => {
+	it("flattens an array of objects", async () => {
+		const source = {
+			key_one: "hello",
+			key_two: { subkey_one: "world", subkey_two: { x: 4 } },
+			key_three: [{ key: "the_key", value: "the_value" }]
+		};
+		const target = {
+			keyOne: "hello",
+			keyThree0Key: "the_key",
+			keyThree0Value: "the_value",
+			keyTwoSubkeyOne: "world",
+			keyTwoSubkeyTwoX: 4
+		};
+		expect([target, target]).to.deep.equal(flattenObjects([source, source]));
+	});
+});
